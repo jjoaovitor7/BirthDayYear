02375dbce43b080fc51d7775e5245258b0e121f8
João Vítor <joaovitorsf7@hotmail.com>
2021-01-19 12:01:32 -0300

    fixes e comentários.

--- src/BirthDayYear.js
+++ src/BirthDayYear.js
@@ -44,20 +44,42 @@ class BirthDayYear {
     const currentMonth = new Date().getMonth();
     const currentDay = new Date().getDate();
 
+    // se os atributos day, month e age não estiverem definidos,
+    // ou seja, talvez não atenderam os requisitos anteriores.
     if (
-      this.getMonth == undefined ||
       this.getDay == undefined ||
+      this.getMonth == undefined ||
       this.getAge == undefined
     ) {
       console.log("Um dos atributos (dia, mês e idade) não foi definido.");
       return 0;
-    } else {
+    }
+
+    // se os atributos day, month e age estiverem definidos,
+    // ou seja, atenderam os requisitos anteriores.
+    else {
+      // se o mês inserido for menor que o mês atual
+      // e o dia inserido for menor que o dia atual.
       if (this.getMonth <= currentMonth + 1 && this.getDay < currentDay) {
-        return currentYear - this.getAge - 1;
-      } else {
+        return currentYear - this.getAge;
+      }
+
+      // se o mês inserido for maior que o mês atual
+      // e o dia inserido for maior que o dia atual.
+      else {
         const daysOfMonth = new Date(currentYear, currentMonth, 0).getDate();
+
+        // se o dia inserido for menor que a quantidade de dias do mês atual
         if (this.getDay <= daysOfMonth) {
-          return currentYear - this.getAge - 1;
+          // e se o dia for menor ou igual ao dia atual.
+          if (this.getDay <= currentDay) {
+            return currentYear - this.getAge - 1;
+          }
+          
+          // e se o dia for maior ao dia atual.
+          else {
+            return currentYear - this.getAge;
+          }
         } else {
           // throw "Error.";
           console.log("Error.");
